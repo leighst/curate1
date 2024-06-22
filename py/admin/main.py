6cb5c22aaf0b751415054fb1813fba2db2092531
Leigh Stewart <agrodellic@gmail.com>
2024-06-22 09:29:05 -0700

    add

---
+++ py/admin/main.py
@@ -0,0 +1,33 @@
+import argparse
+
+from curate1.resources.database.database import Database
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Hacker News Data Ingestion Tool")
+  subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+  # Command 'db'
+  db_parser = subparsers.add_parser('db', help="Database operations")
+  db_subparsers = db_parser.add_subparsers(dest="db_command", help="Database commands")
+
+  # Command 'db apply'
+  db_push_parser = db_subparsers.add_parser('apply', help="Push data to the database")
+  db_push_parser.add_argument('--db-path', type=str, required=True, help="Path to the database")
+  args = parser.parse_args()
+
+  if args.command == 'db':
+    handle_db_command(parser, args, args.db_command)
+  else:
+    parser.print_help()
+
+  
+def handle_db_command(parser: argparse.ArgumentParser, args: argparse.Namespace, db_command: str): 
+  if db_command == 'apply':
+    db = Database(args.db_path)
+    db.recreate_db()
+  else:
+    parser.print_help()
+
+if __name__ == "__main__":
+  main()
